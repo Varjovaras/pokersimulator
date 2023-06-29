014f72a803b0ac030c53ab6a4effce21e435b960
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2023-06-29 11:45:05 +0300

    make a simple deck of cards

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,106 @@
+#[derive(Debug, Clone, Copy)]
+enum Suit {
+    Hearts,
+    Diamonds,
+    Spades,
+    Clubs,
+}
+
+const SUITS: [Suit; 4] = [Suit::Hearts, Suit::Diamonds, Suit::Clubs, Suit::Spades];
+
+#[derive(Debug, Clone, Copy)]
+enum Value {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+impl Value {
+    fn self_value(&self) -> i32 {
+        match *self {
+            Value::Two => 0,
+            Value::Three => 1,
+            Value::Four => 2,
+            Value::Five => 3,
+            Value::Six => 4,
+            Value::Seven => 5,
+            Value::Eight => 6,
+            Value::Nine => 7,
+            Value::Ten => 8,
+            Value::Jack => 9,
+            Value::Queen => 10,
+            Value::King => 11,
+            Value::Ace => 12,
+        }
+    }
+
+    fn from_char(ch: char) -> Result<Value, &'static str> {
+        let rank = match ch {
+            '2' => Value::Two,
+            '3' => Value::Three,
+            '4' => Value::Four,
+            '5' => Value::Five,
+            '6' => Value::Six,
+            '7' => Value::Seven,
+            '8' => Value::Eight,
+            '9' => Value::Nine,
+            'T' => Value::Ten,
+            'J' => Value::Jack,
+            'Q' => Value::Queen,
+            'K' => Value::King,
+            'A' | '1' => Value::Ace,
+            _ => return Err("Invalid rank"),
+        };
+        Ok(rank)
+    }
+}
+
+const VALUES: [Value; 13] = [
+    Value::Ace,
+    Value::Two,
+    Value::Three,
+    Value::Four,
+    Value::Five,
+    Value::Six,
+    Value::Seven,
+    Value::Eight,
+    Value::Nine,
+    Value::Ten,
+    Value::Jack,
+    Value::Queen,
+    Value::King,
+];
+
+#[derive(Debug)]
+struct Card {
+    suit: Suit,
+    value: Value,
+}
+
+impl Card {
+    /// Creates a card with the given suit and rank
+    fn new(suit: Suit, value: Value) -> Card {
+        Card { value, suit }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cards = Vec::new();
+    for suit in SUITS {
+        for value in VALUES {
+            let card = Card::new(suit, value);
+            cards.push(card);
+        }
+    }
+    println!("{:#?}", cards);
 }
